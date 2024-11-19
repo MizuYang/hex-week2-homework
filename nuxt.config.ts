c62c37e1d40bfdc5f14bf5b642bfeac380f8b8c4
mizu <psdk55052004@gmail.com>
2024-11-19 23:58:25 +0800

    新增 加入全域樣式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // 全域的樣式
+  css: [
+    '@/assets/styles/all.scss'
+  ]
 })
